4ca0fda3cefb67dc14e2b2d54b4c09eccb87db6f
Fabian <125907927+mskiFabian@users.noreply.github.com>
2023-04-14 19:03:47 +0200

    Add files via upload

---
+++ cd.js
@@ -0,0 +1,10 @@
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+
+function noOdds( values ){
+  // Return all non-odd values
+  let noOdd = values.filter(n => n % 2 === 0)
+  return noOdd
+}
